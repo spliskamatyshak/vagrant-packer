5f57d7adf9eca3c43088ca6478c0329bf4401398
Stephen Pliska-Matyshak <stephen.pliska-matyshak@kohls.com>
2019-03-28 13:51:07 -0500

    Update make.py

--- make.py
+++ make.py
@@ -9,21 +9,25 @@ def build(name):
     """ Build virtualbox vagrant box image """
     try:
         subprocess.run(["packer", "build", "-force", name])
+        return 0
     except subprocess.CalledProcessError as err:
         print('Error:', err)
+        return 1
 
 def add(name):
     """ Add vagrant box image to vagrant """
     try:
         subprocess.run(["vagrant", "box", "add", "--force", "--name=" + name,
                         "packer_cent7_virtualbox.box"])
+        return 0
     except subprocess.CalledProcessError as err:
         print('Error:', err)
+        return 1
 
 def main():
     """ Main function """
-    build("vbox.json")
-    add("CentOS")
+    if build("vbox.json"):
+        add("CentOS")
 
 if __name__ == "__main__":
     main()
