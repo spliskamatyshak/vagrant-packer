f61541c69976ab8b7856b4209fdefd0f99253d87
Stephen Pliska-Matyshak <stephen.pliska-matyshak@kohls.com>
2019-04-04 17:42:41 -0500

    add kitchen tests

--- test/integration/default/tests.rb
+++ test/integration/default/tests.rb
@@ -1,3 +1,17 @@
-describe package('python-pip') do
+describe package('epel-release') do
   it { should be_installed }
+end
+    
+describe package('python2-pip') do
+  it { should be_installed }
+end
+    
+describe pip('pip') do
+  it { should be_installed }
+  its('version') { should eq '19.0.3' }
+end
+    
+describe pip('ansible') do
+  it { should be_installed }
+  its('version') { should eq '2.7.10' }
 end
